eba7eed0c5babf316b122a297a11c1b3ba904ca6
anoriqq <shota.yoshikawa@anoriqq.com>
2020-02-15 16:28:33 +0900

    chore: completed lesson 1

--- 1-navigate-between-pages/pages/index.js
+++ 1-navigate-between-pages/pages/index.js
@@ -1,6 +1,11 @@
+import Link from 'next/link';
+
 export default function Index() {
   return (
     <div>
+      <Link href="/about" title="About Page">
+        <a>About Page</a>
+      </Link>
       <p>Hello Next.js</p>
     </div>
   )
